48f2a8a763cbd98a23d678f55090143802cefc7d
LDH9276 <leedh9276@naver.com>
2026-05-30 09:53:48 +0900

    fix: Scroll 인디케이터 겹침 해소 및 모바일 숨김 처리

--- src/Header/Scroll.jsx
+++ src/Header/Scroll.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Scroll() {
   return (
-    <div className="absolute bottom-8 left-1/2 -translate-x-1/2 scroll-indicator">
+    <div className="absolute bottom-6 left-1/2 -translate-x-1/2 scroll-indicator z-20 pointer-events-none hidden md:flex">
       <div className="scroll-indicator-bar" />
       <span className="scroll-indicator-text">scroll</span>
     </div>
